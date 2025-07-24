b967debcc2a4f91cd9506787543eb33323b0014a
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-07-24 18:20:41 +0530

    Need to work on cache and segment concepts in Next.js

---
+++ nextapp/app/lectures/[...slug]/page.tsx
@@ -0,0 +1,17 @@
+export default async function Lectures({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>;
+}) {
+  const { slug } = await params;
+
+  console.log(slug);
+
+  if (slug.length == 2) {
+    return <div>Lecture 1</div>;
+  }
+
+  if (slug.length == 3) {
+    return <div>Lecture 2</div>;
+  }
+}
