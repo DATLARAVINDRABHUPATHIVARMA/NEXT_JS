801fb6e59e14e61be434f3eae347f14ff7a57753
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-07-29 16:33:02 +0530

    Start with Active links

--- nextapp/app/(Home)/Product/page.tsx
+++ nextapp/app/(Home)/Product/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function Products() {
+    const ProductID = 4
     return (
         <div>
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                <li><Link href="/Product/1">Product 1</Link></li>
+                <li><Link href="/Product/2">Product 2</Link></li>
+                <li><Link href="/Product/3" replace>Product 3</Link></li>
+                <li><Link href={`/Product/${ProductID}`}>Product {ProductID}</Link></li>
             </ul>
         </div>
     )
--- nextapp/app/(Home)/contact/page.tsx
+++ nextapp/app/(Home)/contact/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata:Metadata = {
+  title:{
+    absolute: "Contact Next.js"
+  }
+}
+
 export default function Contact() {
  return(<h1>Contactt</h1>)
 }
