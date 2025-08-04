1408ba8d42c4b50c1905f7b1276f6c58ac0de86a
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-08-04 18:31:12 +0530

    error handling including nested done

--- nextapp/app/(Home)/Product/[ProductID]/reviews/[reviewID]/page.tsx
+++ nextapp/app/(Home)/Product/[ProductID]/reviews/[reviewID]/page.tsx
@@ -1,10 +1,15 @@
+
 import {notFound, redirect} from "next/navigation"
 
 export default async function ReviewDetails({params}:{params:Promise<{ProductID: string, reviewID: string}>}){
     const {ProductID, reviewID} = await params
 
-    if(reviewID > "50"){
+    if(reviewID > "60"){
         redirect("/")
+    }
+
+    if(reviewID == "50"){
+        throw new Error("ReviewID Error")
     }
     
     return (<h2>Product ID {ProductID} Review ID {reviewID}</h2>)
