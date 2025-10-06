d9f8e14dfa9e9b9b2e67145d30af659b52eb1907
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-10-05 23:45:27 -0500

    perfect schedule

--- nextapp/app/layout.tsx
+++ nextapp/app/layout.tsx
@@ -1,39 +1,43 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react"; 
+import { useState } from "react";
 import { ErrorWrapper } from "./GlobalError";
-
 
 const navigations = [
   { title: "About", href: "/about" },
   { title: "Products", href: "/Product" },
   { title: "Blog", href: "/blog" },
   { title: "Contact", href: "/contact" },
-]
+];
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-  const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body>
         <div>
-          {navigations.map(nav => {
-            const isActive = pathname === nav.href || (pathname.startsWith(nav.href) && nav.href !== "page")
-            return(
-              <Link className={isActive ? "to-blue-700" : ""} key={nav.title} href={nav.href}>{nav.title}</Link>
-            )
-          })} 
+          {navigations.map((nav) => {
+            const isActive =
+              pathname === nav.href ||
+              (pathname.startsWith(nav.href) && nav.href !== "page");
+            return (
+              <Link
+                className={isActive ? "to-blue-700" : ""}
+                key={nav.title}
+                href={nav.href}
+              >
+                {nav.title}
+              </Link>
+            );
+          })}
         </div>
-        <ErrorWrapper>
-          {children}
-        </ErrorWrapper>
+        <ErrorWrapper>{children}</ErrorWrapper>
       </body>
     </html>
-  )
+  );
 }
