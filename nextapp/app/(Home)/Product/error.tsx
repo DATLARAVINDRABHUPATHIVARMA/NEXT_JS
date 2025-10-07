d5cd999e6e0d72e13e3840a0f019eed1c8ebfb68
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-10-06 23:13:01 -0500

    prep sce

--- nextapp/app/(Home)/Product/error.tsx
+++ nextapp/app/(Home)/Product/error.tsx
@@ -1,15 +1,9 @@
-"use client";
-export default function ReviewError({
-  error,
-  reset,
-}: {
-  error: Error;
-  reset: () => void;
-}) {
+"use client"
+export default function ReviewError({error, reset}:{error: Error, reset: () => void}){
   return (
     <div>
       <p>Error: {error.message}</p>
       <button onClick={reset}>Refresh</button>
     </div>
-  );
+  )
 }
