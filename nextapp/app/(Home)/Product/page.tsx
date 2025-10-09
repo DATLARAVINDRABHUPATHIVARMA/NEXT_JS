02be44631966bdd4985fea6afdc7a33df71df0e6
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-10-09 12:25:20 -0500

    apollo

--- nextapp/app/(Home)/Product/page.tsx
+++ nextapp/app/(Home)/Product/page.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
 
 export default function Products() {
-    const ProductID = 4
-    return (
-        <div>
-            <ul>
-                <li><Link href="/Product/1">Product 1</Link></li>
-                <li><Link href="/Product/2">Product 2</Link></li>
-                <li><Link href="/Product/3" replace>Product 3</Link></li>
-                <li><Link href={`/Product/${ProductID}`}>Product {ProductID}</Link></li>
-            </ul>
-        </div>
-    )
+  const ProductID = 4;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/Product/1">Product 1</Link>
+        </li>
+        <li>
+          <Link href="/Product/2">Product 2</Link>
+        </li>
+        <li>
+          <Link href="/Product/3" replace>
+            Product 3
+          </Link>
+        </li>
+        <li>
+          <Link href={`/Product/${ProductID}`}>Product {ProductID}</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
