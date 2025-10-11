d48f18e1e4a273ffcbecb2cf9f56e66d157d9212
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-10-11 02:00:45 -0500

    ms pan

--- nextapp/app/GlobalError.tsx
+++ nextapp/app/GlobalError.tsx
@@ -1,34 +1,34 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
-interface WrapperProps{
+interface WrapperProps {
   children: React.ReactNode;
 }
 
-const ErrorSimulator =({
+const ErrorSimulator = ({
   message = "An Error Occured",
 }: {
-  message?: string; 
+  message?: string;
 }) => {
   const [error, setError] = useState(false);
 
-  if(error) throw new Error(message);
+  if (error) throw new Error(message);
 
-  return(
-    <button title="Simulate an Error" onClick={()=>setError(true)}>
+  return (
+    <button title="Simulate an Error" onClick={() => setError(true)}>
       Simulate Error
     </button>
-  )
-}
+  );
+};
 
-export const ErrorWrapper = ({children}:WrapperProps) => {
-  return(
+export const ErrorWrapper = ({ children }: WrapperProps) => {
+  return (
     <div>
       <div>
-        <ErrorSimulator message="Error in Layout"/>
+        <ErrorSimulator message="Error in Layout" />
       </div>
       {children}
     </div>
-  )
-}
+  );
+};
