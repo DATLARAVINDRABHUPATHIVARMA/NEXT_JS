89a0994ee89689d8ce8b0366447835ddf8eefc0a
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-10-12 20:25:11 -0500

    ch house shift

--- nextapp/app/(Home)/Product/[ProductID]/layout.tsx
+++ nextapp/app/(Home)/Product/[ProductID]/layout.tsx
@@ -1,19 +1,22 @@
-function getError(count: number){
-  return Math.floor(Math.random() * count)
+function getError(count: number) {
+  return Math.floor(Math.random() * count);
 }
 
-export default function ProductLayout({children}:{children: React.ReactNode}){
-  const err = getError(2)
+export default function ProductLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const err = getError(2);
 
-  if(err == 1){
-    throw new Error("Error in layout")
+  if (err == 1) {
+    throw new Error("Error in layout");
   }
 
-  return(
+  return (
     <div>
       {children}
       <h3>Featured Products</h3>
     </div>
-  )
-  
+  );
 }
