92da2d3106f4c5fa68d652649d93f5d47a2eca80
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-10-19 22:13:07 -0500

    next status pause

--- nextapp/app/(Home)/Product/error.tsx
+++ nextapp/app/(Home)/Product/error.tsx
@@ -1,9 +1,15 @@
-"use client"
-export default function ReviewError({error, reset}:{error: Error, reset: () => void}){
+"use client";
+export default function ReviewError({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
   return (
     <div>
       <p>Error: {error.message}</p>
       <button onClick={reset}>Refresh</button>
     </div>
-  )
+  );
 }
